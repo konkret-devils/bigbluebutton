1376370ac773dabf523138282011580ae42f2eee
Martin Beckmann <m.beckmann@konkret-mafo.cloud>
2021-01-18 14:57:51 +0100

    //maybe the statement below is deprecated for bbb 2.3-dev ?    //Session.set('openPanel', 'userlist');

--- bigbluebutton-html5/imports/startup/client/base.jsx
+++ bigbluebutton-html5/imports/startup/client/base.jsx
@@ -399,7 +399,8 @@ const BaseContainer = withTracker(() => {
   }
 
   if (getFromUserSettings('bbb_show_participants_on_login', true) && !deviceInfo.type().isPhone) {
-    Session.set('openPanel', 'userlist');
+    //maybe the line below is deprecated for bbb 2.3-dev ?
+    //Session.set('openPanel', 'userlist');
     if (CHAT_ENABLED && getFromUserSettings('bbb_show_public_chat_on_login', !Meteor.settings.public.chat.startClosed)) {
       Session.setDefault('openPanel', 'chat');
       Session.setDefault('idChatOpen', PUBLIC_CHAT_ID);
